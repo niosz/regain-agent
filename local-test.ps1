26bf0bea6191f8d031210159319976881980e814
NiosZ <fabio.gatto@live.it>
2025-07-17 18:00:20 +0200

    5

--- local-test.ps1
+++ local-test.ps1
@@ -5,15 +5,15 @@ $username2 = Read-Host -Prompt "Username 2"
 $password2 = Read-Host -Prompt "Password 2" -AsSecureString | ConvertFrom-SecureString
 $isBasic = "y"
 try {
-    Write-Information "Verifica di connessione utenza 1 : $username1"
+    Write-Host "Verifica di connessione utenza 1 : $username1"
     Connect-ManagementServer -Server $server -Credential ([pscredential]::new($username1, ($password1 | ConvertTo-SecureString))) -BasicUser:($isBasic -eq 'y')
     Disconnect-ManagementServer
-    Write-Information "Verifica di connessione utenza 2 : $username2"
+    Write-Host "Verifica di connessione utenza 2 : $username2"
     Connect-ManagementServer -Server $server -Credential ([pscredential]::new($username2, ($password2 | ConvertTo-SecureString))) -BasicUser:($isBasic -eq 'y')
     Disconnect-ManagementServer
-    Write-Information "Verifiche eseguete con successo."
+    Write-Host "Verifiche eseguete con successo."
 }
 catch {
-    Write-Error "Errore durante la verifica delle credenziali: $_"
+    Write-Host "Errore durante la verifica delle credenziali: $_"
     exit 1
 }
